cb4f221b9f3917b0d2216d8812816c28a811131d
mtaylorpdx <taylor.matt@protonmail.com>
2020-02-28 09:52:23 -0800

    create constructors for bread and pastry classes

--- Models/Orders.cs
+++ Models/Orders.cs
@@ -4,11 +4,20 @@ namespace Bakery.Models
 {
   public class Bread
   {
+    public int BreadQuantity { get; set; }
 
+    public Bread()
+    {
+      BreadQuantity = 0;
+    }
   }
 
   public class Pastry
   {
-
+    public int PastryQuantity { get; set; }
+    public Pastry()
+    {
+      PastryQuantity = 0;
+    }
   }
 }
