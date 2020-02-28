1b9bce7c237d5aeebb8351250dc987b3041b766d
mtaylorpdx <taylor.matt@protonmail.com>
2020-02-28 12:25:11 -0800

    add total sum to output

--- Program.cs
+++ Program.cs
@@ -34,13 +34,14 @@ namespace Bakery
       pastryOrder.SetPastryQuantity(int.Parse(pastryCountString));
     }
 
-      int breadTotal = breadOrder.BreadPriceTotal();
-      int pastryTotal = pastryOrder.PastryPriceTotal();
-      
+    int breadTotal = breadOrder.BreadPriceTotal();
+    int pastryTotal = pastryOrder.PastryPriceTotal();
+
     Console.WriteLine($@"
           Your total order comes to:
-            {breadCountString} loaves: ${breadTotal}
-            {pastryCountString} pastries: ${pastryTotal}
+            {breadCountString} Loaves: ${breadTotal}
+            {pastryCountString} Pastries: ${pastryTotal}
+            Total: ${breadTotal + pastryTotal}
             ");
     }
     static bool NumberCheck(string inputQuantity)
