a736bae1c95cf58403c0ffc6fca8f11bcb6cf7ea
mtaylorpdx <taylor.matt@protonmail.com>
2020-02-28 10:12:14 -0800

    instantiate new objects on program load, pass order quantities into objects

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace Bakery
     {
         static void Main()
         {
+        Pastry pastryOrder = new Pastry();
+        Bread breadOrder = new Bread();
+
             Console.WriteLine(@"
 
                           Welcome to Pierre's!
@@ -16,12 +19,17 @@ namespace Bakery
             #   Buy 2 and Get 1 Free        or 3 for $5      #
             ##################################################
             ");
+
         Console.Write("Enter the number of loaves: ");
         string breadCountString = Console.ReadLine();
         int breadCount = int.Parse(breadCountString);
+        breadOrder.SetBreadQuantity(breadCount);
+        
         Console.Write("Enter the number of pastries: ");
         string pastryCountString = Console.ReadLine();
         int pastryCount = int.Parse(pastryCountString);
+        pastryOrder.SetPastryQuantity(pastryCount);
+        
         Console.WriteLine(breadCount + " " + pastryCount);
         }
     }
